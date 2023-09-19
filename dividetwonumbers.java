af2d0e9ebac94b9745770afbaa53d67c8ba3b0a8
elna-15 <145447868+elna-15@users.noreply.github.com>
2023-09-19 16:30:51 +0530

    division

---
+++ dividetwonumbers.java
@@ -0,0 +1,9 @@
+package javalab;
+
+public class dividetwonumbers {
+	public static void main(String[] args) {
+    float num1=20f,num2=10f;
+    float division=num1/num2;
+    System.out.println("division="+division);
+}
+}
